b5168efb78dd296cdfa0c57d44ed75866c88afab
Venox <109533093+itsvenox@users.noreply.github.com>
2023-12-17 09:40:05 +0100

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -76,7 +76,7 @@ const projects = [
     },
     {
         repoLink: "https://github.com/itsvenox/AMONG-FIGHTER",
-        discordImageLink: "https://media.discordapp.net/attachments/990639796646977556/1185863086373097483/game_screen.png"
+        discordImageLink: "https://media.discordapp.net/attachments/990639796646977556/1185863086373097483/game_screen.png?ex=65912810&is=657eb310&hm=75bff5ea3cda401b49f0cc06a38eb6e2ee8783892936c59c4e34ca3ac0b1034a&=&format=webp&quality=lossless&width=881&height=616"
     },
 ];
 
